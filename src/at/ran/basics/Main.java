d697c6e69efdba732e878e759dbd9649a3e77d28
Fabrice <Fabrice>
2022-10-03 11:43:08 +0200

    letzten aufgaben

---
+++ src/at/ran/basics/Main.java
@@ -0,0 +1,71 @@
+package at.ran.basics;
+import java.util.Random;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("hi");
+
+
+        public class IfClause {
+
+            public static void main (String []args);
+        }
+        System.out.print("Hello");
+        Random random = new Random();
+        int randomNumber = random.nextINT ( bound: 100);
+        System.out.printIn(random);
+
+        if (randomNumber<10){
+            System.output.printIn("Small");
+
+        }
+        else if {
+
+        }
+
+    }
+
+}
+
+
+public class Ecxercise 1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+public class excercise 2{
+
+    public static void main(String[] args) {
+        for (int i = 2; i < 1001; i = i +2) {
+            System.out.println(i);
+        }
+    }
+}
+
+        import java.util.Random;
+
+public class first
+{
+    public static void main(String[] args)
+    {
+        // Erstelle eine Zufallszahl zwischen 0 und 100
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        System.out.println(randomNumber);
+
+        if(randomNumber<20) {
+            System.out.println("Mini");
+        }
+
+        if((randomNumber>=20) && (randomNumber<=50)){
+            System.out.println("Medium");
+        }
+        if(randomNumber>50) {
+            System.out.println("Large");
+        }
+    }
+}
