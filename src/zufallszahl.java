264ec462950af78fafcd044cb7801f90599b05c3
Fabrice <Fabrice>
2022-10-06 08:56:44 +0200

    random numbers

---
+++ src/zufallszahl.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class zufallszahl {
+    public static void main(String[] args) {
+        {
+            // Erstelle eine Zufallszahl zwischen 0 und 100
+            Random random = new Random();
+            int randomNumber = random.nextInt(100);
+            System.out.println(randomNumber);
+
+            if(randomNumber<20) {
+                System.out.println("Mini");
+            }
+
+            if((randomNumber>=20) && (randomNumber<=50)){
+                System.out.println("Medium");
+            }
+            if(randomNumber>50) {
+                System.out.println("Large");
+            }
+        }
+    }
+}
