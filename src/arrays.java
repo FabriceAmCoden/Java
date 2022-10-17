b2cb678d2d0ca53df12392076692b34df998fe4a
Fabrice <Fabrice>
2022-10-17 12:53:57 +0200

    arrays

--- src/arrays.java
+++ src/arrays.java
@@ -3,18 +3,23 @@ public class arrays {
         int[] trhowsArray = new int[6];
         System.out.println(trhowsArray[4]);
         for (int i = 0; i < trhowsArray.length; i++) {
-                System.out.print(trhowsArray[i]);
-                int [][] playground = new int[3][3];
+            System.out.println(trhowsArray[i]);
 
-            for (int[] ints : playground) {
-                
+            int[][] playGround = new int[3][3];
+
+            playGround[0][0] = 1;
+
+            for (int row = 0; row < playGround.length; row++) {
+
+                for (int col = 0; col < playGround.length; col++) {
+                    System.out.print(playGround[row][col] + " ");
+                }
             }
 
 
         }
-
-        }
-
-        
     }
+}
+        
+
 
