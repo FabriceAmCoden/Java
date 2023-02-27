5cdb5a6fc6154d6a37b5b6654a425833ee1015db
Fabrice <Fabrice>
2023-02-27 11:49:26 +0100

    rueckspiegel erstellen

--- src/carold/engine.java
+++ src/carold/engine.java
@@ -3,30 +3,27 @@ package carold;
 public class engine {
     public enum TYPE {DIESEL, GAS}
 
-    Private int ps;
+    private int ps;
 
-    privat bei. Fabrice. carold. Grundlagen. basicCar. Motor. TYPE-Typ; //Diesel oder Benzin
 
-    public Engine(int ps, at. Fabrice. carold. Grundlagen. basicCar. Motor. TYPE-Typ ) {
-        Dies.  ps = ps;
-        Dies.  Typ = Typ;
+    public engine(int ps, private bei) {
+        this.ps = ps;
+        this.at = ;
     }
 
-
     public int getPs() {
-        Rückgabe PS;
+         return getPs();
     }
 
     public void setPs(int ps) {
-        Dies.  ps = ps;
+        this.ps = ps;
     }
 
-    public void setType(at. Fabrice. carold. Grundlagen. basicCar. Motor. TYPE-Typ ) {
-        Dies.  Typ = Typ;
+    public void setType() {
+        this.TYPE = TYPE;
     }
 
-    öffentlich bei. Fabrice. carold. Grundlagen. basicCar. Motor. TYPE getType() {
-        Art der  Rueckgabe;
+    private at.Fabrice. carold.() {
 
     }
 
---
+++ src/carold/rueckspiegelureifen.java
@@ -0,0 +1,40 @@
+package carold;
+
+
+import oo.Car;
+
+public class rueckspiegelureifen {
+    public class Auto {
+        private int anzahlRueckspiegel;
+        private int anzahlReifen;
+
+        public Auto(int anzahlRueckspiegel, int anzahlReifen) {
+            this.anzahlRueckspiegel = anzahlRueckspiegel;
+            this.anzahlReifen = anzahlReifen;
+        }
+
+        public int getAnzahlRueckspiegel() {
+            return anzahlRueckspiegel;
+        }
+
+        public int getAnzahlReifen() {
+            return anzahlReifen;
+        }
+
+        public void fahre() {
+            System.out.println("Das Auto fährt...");
+        }
+
+        public void bremse() {
+            System.out.println("Das Auto bremst...");
+        }
+
+        public static void main(String[] args) {
+            Auto meinAuto =  Car(2,4);
+            System.out.println("Anzahl Rückspiegel: " + meinAuto.getAnzahlRueckspiegel());
+            System.out.println("Anzahl Reifen: " + meinAuto.getAnzahlReifen());
+            meinAuto.fahre();
+            meinAuto.bremse();
+        }
+    }
+}
