474af72f736c13f3197f6be4618adbb277ab603d
Fabrice <Fabrice>
2023-02-27 09:16:36 +0100

    ….

---
+++ src/carold/engine.java
@@ -0,0 +1,35 @@
+package carold;
+
+public class engine {
+    public enum TYPE {DIESEL, GAS}
+
+    Private int ps;
+
+    privat bei. Fabrice. carold. Grundlagen. basicCar. Motor. TYPE-Typ; //Diesel oder Benzin
+
+    public Engine(int ps, at. Fabrice. carold. Grundlagen. basicCar. Motor. TYPE-Typ ) {
+        Dies.  ps = ps;
+        Dies.  Typ = Typ;
+    }
+
+
+    public int getPs() {
+        Rückgabe PS;
+    }
+
+    public void setPs(int ps) {
+        Dies.  ps = ps;
+    }
+
+    public void setType(at. Fabrice. carold. Grundlagen. basicCar. Motor. TYPE-Typ ) {
+        Dies.  Typ = Typ;
+    }
+
+    öffentlich bei. Fabrice. carold. Grundlagen. basicCar. Motor. TYPE getType() {
+        Art der  Rueckgabe;
+
+    }
+
+
+}
+}
